4e8848aabdb5ec5b7dee0156496601a2c106e185
7777777_4547 <83630775+7777777-4547@users.noreply.github.com>
2025-08-06 17:26:19 +0800

    change: add debug log in event system

--- PackWrapper/Utils.py
+++ PackWrapper/Utils.py
@@ -54,17 +54,20 @@ class Event:
     
     @classmethod
     def subscribe(cls, event_type, callback):
+        Logger.debug(f"Subscribing to event \"{event_type}\" and callback \"{callback}\"")
 
         cls._subscribers.setdefault(event_type, []).append(callback)
     
     @classmethod
     def unsubscribe(cls, event_type, callback):
+        Logger.debug(f"Unsubscribing from event \"{event_type}\" and callback \"{callback}\"")
 
         if event_type in cls._subscribers:
             cls._subscribers[event_type].remove(callback)
     
     @classmethod
     def emit(cls, event_type, data=None):
+        Logger.debug(f"Event \"{event_type}\" emitted with data: \"{data}\"")
 
         for callback in cls._subscribers.get(event_type, []):
             try:
