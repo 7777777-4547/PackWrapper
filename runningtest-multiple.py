71644bc82b007ab53a6659b20ac9eecf2880ea1a
7777777_4547 <83630775+7777777-4547@users.noreply.github.com>
2025-08-08 15:49:31 +0800

    change: a test

--- runningtest-multiple.py
+++ runningtest-multiple.py
@@ -3,4 +3,4 @@ from PackWrapper import *
 config("propertiestest_multiple",debug_mode=True)
 
 for key in get_properties().keys():
-    ResourcepackAuto(get_properties()[key]).export()
+    Resourcepack(**get_properties()[key]).export(export_name = get_properties()[key]["export_name"])
